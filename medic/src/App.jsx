8deafaea842000f845bb3c59b20cb6ff5bf344e7
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-06 12:24:58 +0545

    some components are added.

--- medic/src/App.jsx
+++ medic/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import About from './components/About'
+import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Services from './components/Services'
 
 export default function App() {
   return (
     <>
     <Navbar/>
+    <main>
+      <div id='home'>
+        <Home/>
+      </div>
+      <div id='about'>
+        <About/>
+      </div>
+      <div id='services'>
+        <Services/>
+      </div>
+    </main>
     </>
   )
 }
