0ff52d5d1fea03afb616777f2781acc7b2537a08
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-06 12:24:11 +0545

    some minor updates

--- medic/src/layouts/Button.jsx
+++ medic/src/layouts/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button({title}) {
   return (
     <div>
-        <button className='bg-backGroundColor px-4 py-2 rounded-lg text-white hover:bg-hoverColor transition duration-200 ease-in-out'>
+        <button className='bg-hoverColor px-5 py-3 rounded-lg text-white hover:bg-backGroundColor transition duration-200 ease-in-out'>
             {title}
         </button>
     </div>
