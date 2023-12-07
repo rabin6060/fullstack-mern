7909cd9e7b0899d4554b1328dfd729cb87f5476e
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-07 14:05:44 +0545

    createbooking api is done,

--- backend/controller/booking.controller.js
+++ backend/controller/booking.controller.js
@@ -1,4 +1,18 @@
+import { bookingModel } from "../model/booking.model.js"
 
-export const createBooking =async (req,res) => {
+export const createBooking =async (req,res,next) => {
+    try {
+        const booking = await bookingModel.create(req.body)
+        res.status(200).json(booking)
+    } catch (error) {
+        next(error)
+    }
+}
 
+export const bookingDetails = async (req,res,next)=>{
+    try {
+        const data = await bookingModel.find()
+    } catch (error) {
+        next(error)
+    }
 }
