5f647e6dfef88ba73fee34f7468be11f08a729d4
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-07 13:42:59 +0545

    mongo db connection is established.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import bookRoute from './routes/booking.route.js'
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express()
 
-app.listen(8000,()=>{
-    console.log("server started at 8000 port.");
+const connect = () => {
+     mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+  }
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server started at ${process.env.PORT} port.`);
+    connect()
 })
 
 app.get('/test',(req,res)=>{
     res.status(200).json("test successfull.")
 })
+
+app.use('/api/medic/',bookRoute)
+
