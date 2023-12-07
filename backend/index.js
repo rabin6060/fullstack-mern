e6c96acfc53890f5e2b05f86e085b960a2adad1d
Anand Mainali <anand.mainali@cyberarrow.io>
2023-12-07 13:45:03 +0545

    some error handling middleware is added.

--- backend/index.js
+++ backend/index.js
@@ -27,3 +27,12 @@ app.get('/test',(req,res)=>{
 
 app.use('/api/medic/',bookRoute)
 
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "internal server error."
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
